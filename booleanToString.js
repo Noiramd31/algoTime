665ab3f61e2bd003b5585d492b3b10cc54127428
Noiramd31 <marion2macedo@gmail.com>
2023-03-13 14:44:27 +0100

    3 answers

---
+++ booleanToString.js
@@ -0,0 +1,18 @@
+// function booleanToString(b) {
+//   if (b === true) {
+//     return "true";
+//   } else {
+//     return "false";
+//   }
+// }
+// function booleanToString(b) {
+//   if (b) {
+//     return "true";
+//   }
+//   return "false";
+// }
+
+const booleanToString = (b) => b.toString();
+
+console.log(booleanToString(true));
+console.log(booleanToString(false));
